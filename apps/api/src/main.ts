129bfab67d9792f1d7f8b35f597fc1c60ae66e65
Athos Matos <ladiesman217.as@gmail.com>
2026-05-26 10:43:57 -0300

    initial commit

---
+++ apps/api/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { log } from "@repo/logger";
+import 'dotenv/config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  log(`api running on ${process.env.PORT ?? 3000}`);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
