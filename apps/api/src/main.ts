3a19f33f488aecdcf83a93cde753e81e32ad73f2
Athos Matos <ladiesman217.as@gmail.com>
2026-06-02 12:44:46 -0300

    Add path mappings for constants in TypeScript configuration and update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,11 @@
+import { apiEvents } from '@constants/api/apiEvents';
 import { log } from "@monorepo/logger";
 import { NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  apiEvents
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   log(`api running on ${process.env.PORT ?? 3000}`);
